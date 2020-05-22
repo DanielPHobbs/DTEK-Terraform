5d5b5aefc34040c4f34a5f35ddc847ed089c2a03
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2020-05-22 09:06:35 +0100

    create

--- PJ0025-GrafanaInstall/WinVM_insubnet_inRG.tf
+++ PJ0025-GrafanaInstall/WinVM_insubnet_inRG.tf
@@ -1,9 +1,4 @@
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
 
-variable "prefix" {default = "DtekAZ-TEST"}
 
 #Add variables for Subnet & ResourceGroup
 
