9ba478ee81677b086a8eff543993560f40baf71c
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2020-05-27 21:40:33 +0100

    edit

--- PJ0025-GrafanaInstall/OPTIONS/Deploy-Grafana-mysql.tf
+++ PJ0025-GrafanaInstall/OPTIONS/Deploy-Grafana-mysql.tf
@@ -1,3 +1,5 @@
+#https://www.terraform.io/docs/providers/azurerm/r/mysql_database.html
+
 provider "azurerm" {
   features {}
 }
@@ -9,17 +11,22 @@ data "azurerm_resource_group" "rg" {
 
 
 # Create mysql Instance 
-resource "azurerm_mysql_server" "grafanaDB" {
+resource "azurerm_mysql_server" "grafanasrv" {
   name                = "Grafana-mysqlserver"
   location            = "${var.location}"
   resource_group_name = "${data.azurerm_resource_group.rg.name}"
 
-  administrator_login          = "mysqladminun"
+  administrator_login          = "grafanamysqladmin"
   administrator_login_password = "H@Sh1CoR3!"
 
   sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
+
+
+  storage_profile {
+    storage_mb            = 5120
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
 
   auto_grow_enabled                 = true
   backup_retention_days             = 7
@@ -28,4 +35,12 @@ resource "azurerm_mysql_server" "grafanaDB" {
   public_network_access_enabled     = false
   ssl_enforcement_enabled           = true
   ssl_minimal_tls_version_enforced  = "TLS1_2"
+}
+
+resource "azurerm_mysql_database" "GrafanaDB" {
+  name                = "grafanadb"
+  resource_group_name = "${data.azurerm_resource_group.rg.name}"
+  server_name         = "${azurerm_mysql_server.grafanasrv.name}"
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
 }
