d39f927b8d0f3f3ebfa1bb83b8f9d762f2090ebe
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2020-07-27 20:10:52 +0100

    edit

--- PJ0025-GRAFANA-HA/GRAFANA-HA-AS-LB-02/Using-Keyvault.tf
+++ PJ0025-GRAFANA-HA/GRAFANA-HA-AS-LB-02/Using-Keyvault.tf
@@ -9,8 +9,33 @@ vault_uri = "https://msdn-keyvault.vault.azure.net/"
 }
 
 
+output "vm-user" {
+  value = data.azurerm_key_vault_secret.dtek-VM-User.value
+output "vm-password" {
+  value = data.azurerm_key_vault_secret.dtek-VM-password.value
+
+
+/*
+
 os_profile {
 computer_name = "myvm"
-admin_username = "${data.azurerm_key_vault_secret.dtek-VM-User.value}"
-admin_password = "${data.azurerm_key_vault_secret.dtek-VM-password.value}"
+admin_username = data.azurerm_key_vault_secret.dtek-VM-User.value
+admin_password = data.azurerm_key_vault_secret.dtek-VM-password.value
 }
+
+
+
+
+
+###############################################################################
+
+data "azurerm_key_vault_secret" "example" {
+  name         = "secret-sauce"
+  key_vault_id = data.azurerm_key_vault.existing.id
+}
+
+output "secret_value" {
+  value = data.azurerm_key_vault_secret.example.value
+}
+
+*/
