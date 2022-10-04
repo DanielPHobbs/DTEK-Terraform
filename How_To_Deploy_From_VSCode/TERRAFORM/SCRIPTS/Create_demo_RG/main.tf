f78ebb016b4d07b8497c352720c2ec39a90ea4d6
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2022-10-04 15:02:19 +0100

    commit

--- How_To_Deploy_From_VSCode/TERRAFORM/SCRIPTS/Create_demo_RG/main.tf
+++ How_To_Deploy_From_VSCode/TERRAFORM/SCRIPTS/Create_demo_RG/main.tf
@@ -1,22 +1,12 @@
-# 1. Specify the version of the AzureRM Provider to use
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=3.0.1"
-    }
-  }
-}
-
-# 2. Configure the AzureRM Provider
 provider "azurerm" {
-  features {}
+features {}
 }
 
-# 3. Create a resource group
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = "Dtek-Terraform-resources"
-  location = "West Europe"
-
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
-
