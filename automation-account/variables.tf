62c068c3223d24306bb3940d6ed725ed94a9d35f
DTEKITINNOVATIONS GITHUB REPOSITORY <Daniel.hobbs@dtekitinnovations.com>
2022-10-05 08:12:08 +0100

    commit

--- automation-account/variables.tf
+++ automation-account/variables.tf
@@ -1,7 +1,9 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default="PE_BA_UK"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default="West Europe"
 }
